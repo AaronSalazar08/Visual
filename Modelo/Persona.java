bac9b3b0bc7675e64b3ac55d6fafbf47812494c5
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-04-20 18:53:08 -0600

    cambios

--- Modelo/Persona.java
+++ Modelo/Persona.java
@@ -1,19 +1,15 @@
 public class Persona {
 
-//Atributos
-private String nombre;
-private int edad;
-private int cantidad_dedos;
+    // Atributos
+    private String nombre;
+    private int edad;
+    private int cantidad_dedos;
 
+    // Constructor
+    public Persona(String nombre, int edad, int cantidad_dedos) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cantidad_dedos = cantidad_dedos;
+    }
 
-//Constructor
-public Persona(String nombre, int edad, int cantidad_dedos) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.cantidad_dedos = cantidad_dedos;
 }
-
-}
-
-
-
