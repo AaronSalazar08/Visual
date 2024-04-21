4adba1af5ae33b891da1216f2464dc2bd31b7512
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-04-20 19:22:37 -0600

    Se agregaron atributos y el construcor a la clase estudiante, hija de Persona

--- Modelo/Estudiante.java
+++ Modelo/Estudiante.java
@@ -1,3 +1,13 @@
-public class Estudiante {
+public class Estudiante extends Persona {
+
+
+//Atributos
+protected String carnet;
+protected double beca;
+
+public Estudiante(String nombre, int edad, int cantidad_dedos) {
+    super(nombre, edad, cantidad_dedos);
+    //TODO Auto-generated constructor stub
+}
 
 }
