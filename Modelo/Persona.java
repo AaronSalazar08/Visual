2ce97cddd31c683179b9e38e92f67faf77943ecf
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-04-20 18:49:15 -0600

    Constructor

--- Modelo/Persona.java
+++ Modelo/Persona.java
@@ -5,5 +5,12 @@ private String nombre;
 private int edad;
 private int cantidad_dedos;
 
+//Constructor
+public Persona(String nombre, int edad, int cantidad_dedos) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.cantidad_dedos = cantidad_dedos;
+}
+
 
 }
