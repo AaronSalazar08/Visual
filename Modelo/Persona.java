60633402fe52420035ccbdc219d017743a4ac1d0
MayloParra <a494256@gmail.com>
2024-04-20 18:51:56 -0600

    getter y setters

--- Modelo/Persona.java
+++ Modelo/Persona.java
@@ -8,4 +8,30 @@ private int cantidad_dedos;
 public Persona(){
     
 }
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public int getEdad() {
+    return edad;
+}
+
+public void setEdad(int edad) {
+    this.edad = edad;
+}
+
+public int getCantidad_dedos() {
+    return cantidad_dedos;
+}
+
+public void setCantidad_dedos(int cantidad_dedos) {
+    this.cantidad_dedos = cantidad_dedos;
+}
+
+
 }
