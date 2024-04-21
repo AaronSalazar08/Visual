3f8525a45cca43dfed3132d816e94a5a18c1c951
MayloParra <a494256@gmail.com>
2024-04-20 18:32:35 -0600

    agrego un boton jajaja

--- Vista/Vista/Ventana1.java
+++ Vista/Vista/Ventana1.java
@@ -1,13 +1,20 @@
 package Vista;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class Ventana1 extends JFrame
 {
-
+     JPanel panel;
+     JButton boton;
     public Ventana1(){
-
-
+panel=new JPanel();
+boton=new JButton();
         setSize(1000,700);
-        
+        add(panel);
+        panel.setLayout(null);
+        panel.add(boton);
+        boton.setBounds(500, 500, 150, 40);
+        boton.setText("Hola Aaron");
         this.setLocationRelativeTo(null);
     }
 
